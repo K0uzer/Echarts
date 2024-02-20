8ed3f2c6f29cec8fc201dd1985880aa7faebb9b3
Alex <87208475+PuSh0chek@users.noreply.github.com>
2024-02-20 16:14:56 +0300

    Рабочий интерфейс кроме индетификатора валют.

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,77 @@
-import React from 'react';
+import { useState } from 'react';
 import { ReactECharts } from './Echarts/ReactECharts';
+import { mockData } from './data/data';
 
-const option = {
-  xAxis: {
-    type: 'category',
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  series: [
-    {
-      data: [100, 230, 224, 218, 135, 147, 260],
-      mooth: true,
-      type: 'line'
-    }
-  ]
-};
+const dollar = mockData.filter((_, index) => index !< 8)
+const euro = mockData.filter((_, index) => index !< 16 && index !> 8)
+const yuan = mockData.filter((_, index) => index !< 24 && index !> 16)
+
+const currency = ['$', '€', '¥']
 
 function App() {
 
-  const active = () => {
-    
+  const [chart, setChart] = useState(dollar)
+
+  const indicator = chart.map((e) => e.indicator)
+  const value = chart.map((e) => e.value)
+  const data = chart.map((e) => e.month)
+
+  const averageValue = Math.round((chart.map((e) => e.value).reduce((accumulator, currentValue) => accumulator + currentValue) / chart.length) * 10) / 10
+
+  const changeChart = (index:number) => {
+    if(index === 0) setChart(dollar)
+    if(index === 1) setChart(euro)
+    if(index === 2) setChart(yuan)
   }
 
+  const currencySign = 1
+
+  const option = {
+    title: {
+      text: `${indicator[0]}, ${currencySign}/₽`
+    },
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {},
+    toolbox: {
+      show: false,
+      feature: {
+        dataView: { readOnly: false },
+        magicType: { type: ['line', 'bar'] },
+        restore: {},
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: data,
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        name: indicator,
+        type: 'line',
+        data: value,
+      },
+    ]
+  };
+
   return (
-    <div style={{position: 'relative', width:'100vw', height: '100vh'}}>
-      <div style={{font:'inter', fontWeight:'700px', fontSize:'20px', lineHeight:'30px', textTransform:'uppercase'}}>
-        <p style={{position:'absolute'}}><strong>Курс Доллара, $/₽</strong></p>
-        <p style={{position:'absolute'}}><strong>Курс Евро, €/₽ </strong></p>
-        <p style={{position:'absolute'}}><strong>Курс Юань, ¥/₽ </strong></p>
-      </div>
+    <div style={{display: 'flex'}}>
       <div style={{zIndex: 99, position:'absolute', top: '1%', right: '3%'}}>
-        <button onClick={() => active} style={{width:'99px', height: '24px', cursor: 'pointer', borderRadius:'4px' , background: '#00426947' }}>$</button>
-        <button onClick={() => active} style={{width:'99px', height: '24px', cursor: 'pointer' }}>€</button>
-        <button onClick={() => active} style={{width:'99px', height: '24px', cursor: 'pointer' }}>¥</button>
+        {currency.map((e, index) => <button key={index} onClick={() => changeChart(index)} style={{width:'99px', height: '24px', cursor: 'pointer' }}>{e}</button>)}
       </div>
-      <Container />
+      <ReactECharts style={{width:'100vw', height: '100vh'}} option={option}/>
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+        <p>Среднее за период</p>
+        <p>{averageValue}</p>
+      </div>
     </div>
     );
-}
-
-const Container = () => {
-  return ( <div><ReactECharts style={{width:'100vw', height: '100vh'}} option={option}/></div> )
 }
 
 export default App;
